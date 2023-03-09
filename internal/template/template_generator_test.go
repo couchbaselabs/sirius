f6ebb3385b66420b093b5558ada271bb36bae584
Manik Mahajan <manik.mahajan@couchbase.com>
2023-03-09 18:58:49 +0530

    JSON template using jaswdr/faker

--- internal/template/template_generator_test.go
+++ internal/template/template_generator_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestGeneratePerson(t *testing.T) {
-	persons := GeneratePerson(10, 1678361550549466000)
+	persons := GeneratePersons(10, 1678361550549466000)
 	if len(persons) == 0 {
 		t.Fail()
 	}
