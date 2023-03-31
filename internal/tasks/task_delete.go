00fea362b2c7eebae241a3b1d369bfdfb019736f
Manik Mahajan <115541918+manik-couchbase@users.noreply.github.com>
2023-03-31 13:01:34 +0530

    Updates :- (#11)
    1. Replaced Queue system with channels to send burst of documents loading operation.

--- internal/tasks/task_delete.go
+++ internal/tasks/task_delete.go
@@ -1,8 +1,10 @@
 package tasks
 
 import (
+	"fmt"
 	"github.com/couchbase/gocb/v2"
 	"github.com/couchbaselabs/sirius/internal/docgenerator"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"sync"
 )
@@ -23,41 +25,48 @@ func (t *Task) deleteDocuments(gen docgenerator.Generator, col *gocb.Collection)
 		return
 	}
 
-	deleteQueue := make([]int64, 0, MaxConcurrentOps)
-	for i := t.Request.Start; i <= t.Request.End; i++ {
-		deleteQueue = append(deleteQueue, i)
-		if len(deleteQueue) == MaxConcurrentOps || i == t.Request.End {
-			var wg sync.WaitGroup
-			wg.Add(len(deleteQueue))
-			for _, offset := range deleteQueue {
-				go func(offset int64) {
-					defer wg.Done()
-					key := gen.Seed + offset
+	rateLimiter := make(chan struct{}, MaxConcurrentOps)
+	dataChannel := make(chan int64, MaxConcurrentOps)
+	group := errgroup.Group{}
 
-					if _, ok := insertErrorCheck[key]; ok {
-						return
-					}
-					if _, ok := deleteCheck[key]; ok {
-						return
-					}
-					docId := gen.BuildKey(key)
-					if key > t.TaskState.SeedEnd || key < t.TaskState.Seed {
-						t.incrementFailure(key, docId, "docId out of bound")
-						return
-					}
-					_, err := col.Remove(docId, nil)
-					if err != nil {
-						t.incrementFailure(key, docId, err.Error())
-						return
-					}
-					l.Lock()
-					t.TaskState.DeleteTask.Del = append(t.TaskState.DeleteTask.Del, key)
-					l.Unlock()
-				}(offset - 1)
+	for i := t.Request.Start; i <= t.Request.End; i++ {
+		rateLimiter <- struct{}{}
+		dataChannel <- i
+
+		group.Go(func() error {
+			offset := (<-dataChannel) - 1
+			key := gen.Seed + offset
+			if _, ok := insertErrorCheck[key]; ok {
+				<-rateLimiter
+				return fmt.Errorf("key is in InsertErrorCheck")
 			}
-			wg.Wait()
-			deleteQueue = deleteQueue[:0]
-		}
+			if _, ok := deleteCheck[key]; ok {
+				<-rateLimiter
+				return fmt.Errorf("key is delete from the server")
+			}
+
+			docId := gen.BuildKey(key)
+			if key > t.TaskState.SeedEnd || key < t.TaskState.Seed {
+				t.incrementFailure(key, docId, "docId out of bound")
+				<-rateLimiter
+				return fmt.Errorf("docId out of bound")
+			}
+			_, err := col.Remove(docId, nil)
+			if err != nil {
+				t.incrementFailure(key, docId, err.Error())
+				<-rateLimiter
+				return err
+			}
+			l.Lock()
+			t.TaskState.DeleteTask.Del = append(t.TaskState.DeleteTask.Del, key)
+			l.Unlock()
+
+			<-rateLimiter
+			return nil
+		})
 	}
+	_ = group.Wait()
+	close(rateLimiter)
+	close(dataChannel)
 	log.Println(t.Request.Operation, t.Request.Bucket, t.Request.Scope, t.Request.Collection)
 }
--- internal/tasks/task_validate.go
+++ internal/tasks/task_validate.go
@@ -2,13 +2,14 @@ package tasks
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/couchbase/gocb/v2"
 	"github.com/couchbaselabs/sirius/internal/docgenerator"
 	"github.com/couchbaselabs/sirius/internal/template"
 	"github.com/jaswdr/faker"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"math/rand"
-	"sync"
 )
 
 // ValidateDocuments return the validity of the collection using TaskState
@@ -29,68 +30,76 @@ func (t *Task) ValidateDocuments(gen docgenerator.Generator, col *gocb.Collectio
 		deleteCheck[k] = struct{}{}
 	}
 
-	validateQueue := make([]int64, 0, MaxConcurrentOps)
+	rateLimiter := make(chan struct{}, MaxConcurrentOps)
+	dataChannel := make(chan int64, MaxConcurrentOps)
+	group := errgroup.Group{}
+
 	for key := t.TaskState.Seed; key < t.TaskState.SeedEnd; key++ {
-		validateQueue = append(validateQueue, key)
-		if len(validateQueue) == MaxConcurrentOps || key == t.Request.End {
-			var wg sync.WaitGroup
-			wg.Add(len(validateQueue))
-			for _, key := range validateQueue {
-				go func(key int64) {
-					defer wg.Done()
+		rateLimiter <- struct{}{}
+		dataChannel <- key
 
-					if _, ok := insertErrorCheck[key]; ok {
-						return
-					}
-					if _, ok := deleteCheck[key]; ok {
-						return
-					}
-					docId := gen.BuildKey(key)
-					var resultFromHost map[string]interface{}
-					documentFromHost, err := template.InitialiseTemplate(t.Request.TemplateToken)
-					if err != nil {
-						t.validationFailures(docId)
-						return
-					}
-					result, err := col.Get(docId, nil)
-					if err != nil {
-						t.incrementFailure(key, docId, err.Error())
-						return
-					}
-					if err := result.Content(&resultFromHost); err != nil {
-						t.incrementFailure(key, docId, err.Error())
-						return
-					}
-					resultBytes, err := json.Marshal(resultFromHost)
-					err = json.Unmarshal(resultBytes, &documentFromHost)
-					if err != nil {
-						t.validationFailures(docId)
-
-						return
-					}
-					fake := faker.NewWithSeed(rand.NewSource(key))
-					originalDocument, err := gen.Template.GenerateDocument(&fake, t.TaskState.DocumentSize)
-					if err != nil {
-						t.validationFailures(docId)
-
-						return
-					}
-					originalDocument, err = t.retracePreviousMutations(key, originalDocument, gen, &fake)
-					if err != nil {
-						t.validationFailures(docId)
-
-						return
-					}
-					ok, err := gen.Template.Compare(documentFromHost, originalDocument)
-					if err != nil || !ok {
-						t.validationFailures(docId)
-						return
-					}
-				}(key)
+		group.Go(func() error {
+			key := <-dataChannel
+			if _, ok := insertErrorCheck[key]; ok {
+				<-rateLimiter
+				return fmt.Errorf("key is in InsertErrorCheck")
 			}
-			wg.Wait()
-			validateQueue = validateQueue[:0]
-		}
+			if _, ok := deleteCheck[key]; ok {
+				<-rateLimiter
+				return fmt.Errorf("key is delete from the server")
+			}
+			docId := gen.BuildKey(key)
+			var resultFromHost map[string]interface{}
+			documentFromHost, err := template.InitialiseTemplate(t.Request.TemplateToken)
+			if err != nil {
+				t.validationFailures(docId)
+				<-rateLimiter
+				return err
+			}
+			result, err := col.Get(docId, nil)
+			if err != nil {
+				t.incrementFailure(key, docId, err.Error())
+				<-rateLimiter
+				return err
+			}
+			if err := result.Content(&resultFromHost); err != nil {
+				t.incrementFailure(key, docId, err.Error())
+				<-rateLimiter
+				return err
+			}
+			resultBytes, err := json.Marshal(resultFromHost)
+			err = json.Unmarshal(resultBytes, &documentFromHost)
+			if err != nil {
+				t.validationFailures(docId)
+				<-rateLimiter
+				return err
+			}
+			fake := faker.NewWithSeed(rand.NewSource(key))
+			originalDocument, err := gen.Template.GenerateDocument(&fake, t.TaskState.DocumentSize)
+			if err != nil {
+				t.validationFailures(docId)
+				<-rateLimiter
+				return err
+			}
+			originalDocument, err = t.retracePreviousMutations(key, originalDocument, gen, &fake)
+			if err != nil {
+				t.validationFailures(docId)
+				<-rateLimiter
+				return err
+			}
+			ok, err := gen.Template.Compare(documentFromHost, originalDocument)
+			if err != nil || !ok {
+				t.validationFailures(docId)
+				<-rateLimiter
+				return err
+			}
+
+			<-rateLimiter
+			return nil
+		})
 	}
+	_ = group.Wait()
+	close(rateLimiter)
+	close(dataChannel)
 	log.Println(t.Request.Operation, t.Request.Bucket, t.Request.Scope, t.Request.Collection, t.Result.Failure)
 }
