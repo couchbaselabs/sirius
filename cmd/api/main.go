7622cbf042b2b72809d55e1243efbe80c4220b6a
Manik Mahajan <manik.mahajan@couchbase.com>
2023-06-20 14:20:16 +0530

    Updates :- 1. Added debug server at port 6060.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	_ "expvar" // Register the expvar handlers
 	"fmt"
 	"github.com/couchbaselabs/sirius/internal/server_requests"
 	"github.com/couchbaselabs/sirius/internal/sirius_documentation"
 	"github.com/couchbaselabs/sirius/internal/tasks_manager"
+	"github.com/pkg/profile"
 	"io"
 	"log"
 	"net/http"
+	_ "net/http/pprof" // Register the pprof handlers
 	"os"
 )
 
@@ -19,8 +22,24 @@ type Config struct {
 	serverRequests *server_requests.ServerRequests
 }
 
+type DebugServer struct {
+	*http.Server
+}
+
+// NewDebugServer provides new debug http server
+func NewDebugServer(address string) *DebugServer {
+	return &DebugServer{
+		&http.Server{
+			Addr:    address,
+			Handler: http.DefaultServeMux,
+		},
+	}
+}
+
 func main() {
 	registerInterfaces()
+	defer profile.Start(profile.MemProfile).Stop()
+	//gocb.SetLogger(gocb.DefaultStdioLogger())
 
 	logFile, err := os.OpenFile(getFileName(), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
@@ -43,10 +62,18 @@ func main() {
 		Handler: app.routes(),
 	}
 	// start the server
-	err = srv.ListenAndServe()
-	if err != nil {
-		app.taskManager.StopTaskManager()
-		log.Println(err)
-		os.Exit(-1)
-	}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			app.taskManager.StopTaskManager()
+			log.Println(err)
+			os.Exit(-1)
+		}
+	}()
+
+	debugServer := NewDebugServer(fmt.Sprintf("%s:%d", "0.0.0.0", 6060))
+	log.Println("Starting Sirius profiling service at 6060")
+
+	log.Fatal(debugServer.ListenAndServe())
+
 }
