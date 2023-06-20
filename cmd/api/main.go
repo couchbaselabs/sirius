67b15bd43538caec941487a38a734af02e9d6311
Manik Mahajan <manik.mahajan@couchbase.com>
2023-06-20 14:20:16 +0530

    Updates :- 1. Added debug server at port 6060.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	_ "expvar" // Register the expvar handlers
 	"fmt"
+	"github.com/couchbase/gocb/v2"
 	"github.com/couchbaselabs/sirius/internal/generate"
 	"github.com/couchbaselabs/sirius/internal/server_requests"
 	"github.com/couchbaselabs/sirius/internal/tasks-manager"
+	"github.com/pkg/profile"
 	"log"
 	"net/http"
+	_ "net/http/pprof" // Register the pprof handlers
 	"os"
 )
 
@@ -18,16 +22,31 @@ type Config struct {
 	serverRequests *server_requests.ServerRequests
 }
 
+type DebugServer struct {
+	*http.Server
+}
+
+// NewDebugServer provides new debug http server
+func NewDebugServer(address string) *DebugServer {
+	return &DebugServer{
+		&http.Server{
+			Addr:    address,
+			Handler: http.DefaultServeMux,
+		},
+	}
+}
+
 func main() {
 	registerInterfaces()
-	//logger, _ := zap.NewProduction()
-	//defer logger.Sync()
+	defer profile.Start(profile.MemProfile).Stop()
+	gocb.SetLogger(gocb.DefaultStdioLogger())
 
 	app := Config{
 		taskManager:    tasks_manager.NewTasKManager(TaskQueueSize),
 		serverRequests: server_requests.NewServerRequests(),
 	}
 	go generate.Generate()
+
 	//define the server
 	log.Printf("Starting Document Loading Service at port %s\n", webPort)
 	srv := http.Server{
@@ -35,10 +54,18 @@ func main() {
 		Handler: app.routes(),
 	}
 	// start the server
-	err := srv.ListenAndServe()
-	if err != nil {
-		app.taskManager.StopTaskManager()
-		log.Println(err)
-		os.Exit(-1)
-	}
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			app.taskManager.StopTaskManager()
+			log.Println(err)
+			os.Exit(-1)
+		}
+	}()
+
+	debugServer := NewDebugServer(fmt.Sprintf("%s:%d", "0.0.0.0", 6060))
+	log.Println("Starting Sirius profiling service at 6060")
+
+	log.Fatal(debugServer.ListenAndServe())
+
 }
