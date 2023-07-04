caab07b7bf89f6a1ff4c9e4ccaa3da92a8578598
Manik Mahajan <manik.mahajan@couchbase.com>
2023-07-04 15:15:05 +0530

    Updates :- 1. Added run_sirius_latest.sh to support arm based deployment for hub. 2. Added run_sirius_x86.sh to support x86_64 based deployment from hub. 3. Removed Query ping from cluster.go

---
+++ run_sirius_x86.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TASK_RESULT_PATH=./internal/task_result/task_result_logs
+TASK_STATE_PATH=./internal/task_state/task_state_logs
+SERVER_REQUESTS_PATH=./internal/server_requests/server_requests_logs
+TASK_REQUEST_PATH=./internal/tasks/request_logs
+
+echo "Clean meta-data of task state and task results"
+if [ -d ${TASK_RESULT_PATH} ]; then rm -Rf ${TASK_RESULT_PATH}; fi
+if [ -d ${TASK_STATE_PATH} ]; then rm -Rf ${TASK_STATE_PATH}; fi
+if [ -d ${TASK_REQUEST_PATH} ]; then rm -Rf ${TASK_REQUEST_PATH}; fi
+if [ -d ${SERVER_REQUESTS_PATH} ]; then rm -Rf ${SERVER_REQUESTS_PATH}; fi
+echo "Building directory to store task's meta-data and results"
+mkdir -p ${TASK_RESULT_PATH}
+mkdir -p ${TASK_STATE_PATH}
+mkdir -p ${SERVER_REQUESTS_PATH}
+mkdir -p ${TASK_REQUEST_PATH}
+
+docker run -d -p 4000:4000 \
+-v "$(pwd)/internal/tasks/request_logs:/internal/tasks/request_logs" \
+-v "$(pwd)/internal/task_result/task_result_logs:/internal/task_result/task_result_logs" \
+-v "$(pwd)/internal/server_requests/server_requests_logs:/internal/server_requests/server_requests_logs" \
+-v "$(pwd)/internal/task_state/task_state_logs:/internal/task_state/task_state_logs" \
+-d sequoiatools/sirius:x86_64
